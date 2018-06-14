54692d8df85fe0929d06904f6653e8434d25d4f5
macfly <manuel@webdesignord.fr>
2018-06-14 16:31:58 +0200

    add welcome message for OSX user like 2001 the space odyssey

---
+++ welcome_msg.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+usr=$USER
+echo "Écoutez ${usr}, je vois que vous êtes vraiment très affecté par cet incident.\n Et sincèrement je pense que vous devriez reprendre vos esprits,\n absorber un tranquillisant, et essayer de faire le point."
