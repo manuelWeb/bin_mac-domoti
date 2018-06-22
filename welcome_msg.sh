d20668c7618543effa7a8b05716b0915407c5107
macfly <manuel@webdesignord.fr>
2018-06-22 11:05:58 +0200

    welcome_msg.sh add Mr before ${usr}; TODO add 2001 random replica

--- welcome_msg.sh
+++ welcome_msg.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 usr=$USER
-echo "Écoutez ${usr}, je vois que vous êtes vraiment très affecté par cet incident.\n Et sincèrement je pense que vous devriez reprendre vos esprits,\n absorber un tranquillisant, et essayer de faire le point."
+echo "Écoutez Mr ${usr}, je vois que vous êtes vraiment très affecté par cet incident.\n Et sincèrement je pense que vous devriez reprendre vos esprits,\n absorber un tranquillisant, et essayer de faire le point."
